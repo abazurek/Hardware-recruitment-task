067314016bfd61c103535713df22f650a3b887db
abazurek <skala2294@gmail.com>
2022-11-28 20:59:13 +0100

    inicialize

---
+++ Front-end/Debug/habit-app-pure-js/javaScript/add.js
@@ -0,0 +1,34 @@
+import { getHabitList } from './getLocalStorage.js';
+import { render } from './render.js';
+
+const btn = document.querySelector('.btn');
+const input = document.querySelector('#input');
+
+btn.onclick = () => addNew();
+
+input.addEventListener('keyup', (event) => {
+  if (event.keyCode === 13) {
+    addNew();
+  }
+});
+
+function addNew() {
+  const { value: newHabit } = input;
+
+  newHabit && add(newHabit);
+}
+
+function add(newHabit) {
+  const habitList = getHabitList();
+
+  if (habitList && habitList.includes(newHabit)) {
+    alert('Habit exists');
+    return;
+  }
+
+  const newList = habitList ? [...habitList, newHabit] : [newHabit];
+
+  localStorage.setItem('habits', JSON.stringify(newList));
+
+  render();
+}
---
+++ Front-end/Debug/habit-app-pure-js/javaScript/reset.js
@@ -0,0 +1,17 @@
+import { render } from './render.js';
+
+const reset = document.querySelector('.reset');
+
+reset.onclick = () => resetAll();
+
+export function resetAll() {
+  const spinner = document.querySelector('.spinner');
+  const habits = document.querySelector('#habits');
+  habits.innerHTML = '';
+  spinner.classList.remove('d-none');
+  setTimeout(() => {
+    spinner.classList.add('dnone');
+    localStorage.setItem('checkList', JSON.stringify([]));
+    render();
+  }, 400);
+}
